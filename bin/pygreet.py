f90f9fc64a6abe72c71e9beb46ee844e74b9de0b
francosorbello <33555912+francosorbello@users.noreply.github.com>
2019-01-21 18:41:11 -0300

    añadido pygreet

---
+++ bin/pygreet.py
@@ -0,0 +1,21 @@
+import time        
+import click
+import os
+  
+def f(word):
+    for x in word:
+        if x != " ":
+            click.echo('{}{}'.format("", x),nl=False)
+        else:
+            click.echo('{}'.format(" "),nl=False)            
+        time.sleep(0.05)
+
+inittext="Bienvenido, Rulo."       
+f(inittext)
+print("")
+time.sleep(0.05)
+#os.system("cat ~/logo.txt")
+os.system("neofetch")
+f("Presione cualquier tecla para continuar...")
+print("")
+os.system('read -s -n 1 -p ""')  
