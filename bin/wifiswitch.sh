b301a3cdb12f80747b67dd133036e1c56ff7ac77
Franco Sorbello <franco.sorbelo@gmail.com>
2019-04-19 10:17:35 -0300

    Arreglado script que prende y apaga la red. Cambios menores en i3hider

--- bin/wifiswitch.sh
+++ bin/wifiswitch.sh
@@ -3,19 +3,24 @@
 #guardo un valor en sc.txt que me dice si la red esta encendida o no
 #si switch=1,apago y cambio a 0. caso contrario, viceversa
 
-if [ ! [ -e /home/rulo/sc.txt ]]; then
+if [ ! -f /home/rulo/sc.txt ]; then
 	touch /home/rulo/sc.txt
+	echo 0 > /home/rulo/sc.txt
+	echo "archivo sc creado"
 fi
+
 SWITCH=`cat /home/rulo/sc.txt`
 case $SWITCH in
 	0) 
 		nmcli networking on
 		echo 1 > /home/rulo/sc.txt
+		notify-send "Red activada."
 		;;
 
 	1)
 		nmcli networking off
 		echo 0 > /home/rulo/sc.txt
+		notify-send "Red desactivada."
 		;;
 esac
 
